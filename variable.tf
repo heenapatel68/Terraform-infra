208f889185e1a5d6d0d0aed2b8ac3d0dcb123ef7
heenapatel68 <110474587+heenapatel68@users.noreply.github.com>
2026-03-18 01:10:39 -0400

    Define account-replication-type variable with defaults
    Added type and default value for account-replication-type variable.

--- variable.tf
+++ variable.tf
@@ -4,7 +4,10 @@ variable "vnet-name" {}
 variable "address-space" {}
 variable "storage-account-name" {}
 variable "account-tier" {}
-variable "account-replication-type" {}
+variable "account-replication-type" {
+  type    = string
+  default = "LRS
+}
 variable "vm-name" {}
 
 

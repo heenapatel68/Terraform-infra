fee78e1f4fdeb4c121304c217fa68c27608e84a0
heenapatel68 <110474587+heenapatel68@users.noreply.github.com>
2026-03-18 01:14:15 -0400

    Define account-tier variable with type and default
    Add type and default value for account-tier variable

--- variable.tf
+++ variable.tf
@@ -3,7 +3,10 @@ variable "rg-name" {}
 variable "vnet-name" {}
 variable "address-space" {}
 variable "storage-account-name" {}
-variable "account-tier" {}
+variable "account-tier" {
+  type    = string
+  default = "Standard"
+}
 variable "account-replication-type" {
   type    = string
   default = "LRS"

7fa3ad6beb77e8fad580113e087feb578f60e895
heenapatel68 <110474587+heenapatel68@users.noreply.github.com>
2026-03-18 01:15:59 -0400

    Refactor account-tier and account-replication-type variables

--- variable.tf
+++ variable.tf
@@ -3,14 +3,8 @@ variable "rg-name" {}
 variable "vnet-name" {}
 variable "address-space" {}
 variable "storage-account-name" {}
-variable "account-tier" {
-  type    = string
-  default = "Standard"
-}
-variable "account-replication-type" {
-  type    = string
-  default = "LRS"
-}
+variable "account-tier" {}
+variable "account-replication-type" {}
 variable "vm-name" {}
 
 
